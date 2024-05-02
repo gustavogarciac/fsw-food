768cff7984e6642e318c95c785fda569b9b833e0
gustavo <gustavogarciaci1@gmail.com>
2024-05-02 13:41:35 -0300

    feat: loading screen to categories page

---
+++ src/app/categories/[id]/products/loading.tsx
@@ -0,0 +1,36 @@
+import { Header } from '@/components/header'
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+const CategoriesPageLoading = () => {
+  return (
+    <>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-lg font-bold pt-6">Carregando...</h2>
+
+        <CategoriesPageSkeleton />
+      </div>
+    </>
+  )
+}
+
+const CategoriesPageSkeleton = () => {
+  return (
+    <div className="grid grid-cols-2 gap-6 w-full mt-6">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <div key={index} className="flex flex-col w-full gap-2">
+          <Skeleton className="h-52 w-full" />
+          <Skeleton className="h-4 w-1/2" />
+          <div className="flex flex-row gap-1">
+            <Skeleton className="h-4 w-1/4" />
+            <Skeleton className="h-4 w-1/4" />
+          </div>
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default CategoriesPageLoading
